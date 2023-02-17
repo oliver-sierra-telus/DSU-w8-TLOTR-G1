6ddd83534cb77663a04602faf04da02ff7eca17a
ederramos01 <eder.ramos01@telusinternational.com>
2023-02-17 16:03:59 -0600

    testing CombatHistory

---
+++ src/test/java/com/tlotrintf/app/game/CombatHistoryTest.java
@@ -0,0 +1,24 @@
+package com.tlotrintf.app.game;
+
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Test;
+
+public class CombatHistoryTest {
+    @Test
+    public void testShowCombatHistory() {
+        
+        Army armyHeroes = new Army("Heroes Army", 50);
+        CombatHistory combatHistory = mock(CombatHistory.class);
+
+        doNothing().when(combatHistory).showCombatHistory(armyHeroes);
+        combatHistory.showCombatHistory(armyHeroes);
+
+        verify(combatHistory, times(1)).showCombatHistory(armyHeroes);
+
+    }
+}
